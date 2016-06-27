e6558e99dd076af311a6dbd148e135ee0e48c9a8
Chris Prendas <chris.prendas@gmail.com>
2016-06-26 23:08:21 -0600

    proyecto subido

---
+++ webEjemplo/Models/usuarioModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Permite realizar validaciones a nuestra clase a nivel de models
+using System.ComponentModel.DataAnnotations;
+
+namespace webEjemplo.Models
+{
+    public class RegistrarUsuario
+    {
+        [Required]
+        [StringLength(5)]
+        [Display(Name ="Codigo del Cliente")]
+        public int idCliente { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [Display(Name = "Nombre Completo")]
+        public string nomCliente { get; set; }
+
+        [Required]
+        public string cedCliente { get; set; }
+        [Required]
+        public string email { get; set; }
+        [Required]
+        public string direCliente { get; set; }
+        [Required]
+        public string password { get; set; }
+    }
+    public class LogeoUsuario
+    {
+        [Required]
+        public string email { get; set; }
+        [Required]
+        public string password { get; set; }
+
+    }
+    public class CambiarContraseña
+    {
+        [Required]
+        public string password { get; set; }
+    }
+}
---
+++ webEjemplo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(webEjemplo.Startup))]
+namespace webEjemplo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
